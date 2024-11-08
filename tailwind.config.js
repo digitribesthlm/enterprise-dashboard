92cdac709b0bce310cd8bafc6c7065e943df52a3
digitribesthlm <patrik@makeablesthlm.se>
2024-11-08 14:23:40 +0100

    styling

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// File: tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
+}
